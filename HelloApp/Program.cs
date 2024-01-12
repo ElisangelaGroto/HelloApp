2a6d9bb6a0e5aeca37ddc635d13201e1fa1cfd48
Elisangela Groto <elisangelagroto43@gmail.com>
2024-01-11 21:58:30 -0300

    Primeiro commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Imprime um texto na tela.
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("Seja bem vindo ao meu primeiro programa !");
+
+String nome = "Elisangela Groto";
+Console.WriteLine (nome + " é meu nome!");
+
+// Criação de duas variáveis.
+int idade;
+string nome; 
+
+Console.WriteLine ( "Qual é o seu nome?");
+
+// Lê o nome digitado pelo usuário e armazena na variável nome.
+nome = Console.ReadLine();
+
+Console.WriteLine("Prazer em conhecer" + nome + "!"); 
+
+Console.WriteLine( "Qual é a sua idade?");
+
+idade = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Você tem " + idade + "anos de idade.");
+
+
+
